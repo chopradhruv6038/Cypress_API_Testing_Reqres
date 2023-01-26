ff747a00b4a7a3b4921df4061913a526337afe04
chopradhruv6038 <chopradhruv6038@gmail.com>
2023-01-26 16:25:23 +0400

    Configured dashboard and added script to run

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'u2494c',
   reporter: 'cypress-mochawesome-reporter',
   watchForFileChanges: false,
   e2e: {
